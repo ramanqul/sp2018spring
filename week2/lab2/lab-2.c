72788a8dff64aaba40a10f1383b876f1f0518353
Raman Buzaubakov <raman.buzaubakov@gmail.com>
2019-02-08 22:35:55 -0800

    week 2

--- week2/lab2/lab-2.c
+++ week2/lab2/lab-2.c
@@ -1,23 +1,31 @@
-/*  
- *  hello-1.c - The simplest kernel module.
- */
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/module.h>       /* Needed by all modules */
+#include <linux/kernel.h>       /* Needed for KERN_INFO */
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <asm/current.h>
+
+void print_processes(void) {
+	struct task_struct* task = current;
+	printk(KERN_INFO "Begin");
+
+	for_each_process(task) {
+		/* this pointlessly prints the name and PID of each task */
+		printk(KERN_INFO "%s[%d]\n", task->comm, task->pid);
+	}
+
+	printk(KERN_INFO "END");
+}
 
 int init_module(void)
 {
-	printk(KERN_INFO "Lab#2 start.\n");
 
+        printk(KERN_INFO "Lab2: start.\n");
+	print_processes();
 
-	
-
-	/* 
-	 * A non 0 return means init_module failed; module can't be loaded. 
-	 */
-	return 0;
+        return 0;
 }
 
 void cleanup_module(void)
 {
-	printk(KERN_INFO "Lab#2 end.\n");
+        printk(KERN_INFO "Lab2: end.\n");
 }
