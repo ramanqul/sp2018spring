43741291aad1433d73775476a779683a6248acbf
Raman Buzaubakov <raman.buzaubakov@gmail.com>
2019-02-22 22:50:50 -0800

    week3: rbtree working example

--- week3/lab3/lab-3.c
+++ week3/lab3/lab-3.c
@@ -1,7 +1,13 @@
 #include <linux/module.h>       /* Needed by all modules */
 #include <linux/kernel.h>       /* Needed for KERN_INFO */
 #include <linux/slab.h>
+#include <asm/current.h>
 #include <linux/kfifo.h>
+//rbtree example start
+#include <linux/mm.h>
+#include <linux/mm_types.h>
+#include <linux/rbtree.h>
+//rbtree example end
 
 struct mytask_struct {
 	int id;
@@ -19,6 +25,8 @@ void kfifo_example(void) {
 	struct kfifo queue;
 	struct mytask_struct val_mytask;
 	struct mytask_struct *maintask = kmalloc(sizeof(struct mytask_struct), GFP_KERNEL);
+	
+	printk(KERN_NOTICE "---KFIFO example start---");
 
 	maintask->id = 1;
 	maintask->priority = 6;
@@ -55,14 +63,47 @@ void kfifo_example(void) {
 	}
 
 	kfifo_free(&queue);
+
+	printk(KERN_NOTICE "---KFIFO example end---");
 }
 
+void navigate_curr_subtree(struct rb_root *root, struct rb_node *node)
+{
+	int size;
+	struct vm_area_struct *vma;
+
+	if(node->rb_left)
+		navigate_curr_subtree(root, node->rb_left);
+	
+	vma = container_of(node, struct vm_area_struct, vm_rb);		
+	size = (vma->vm_end - vma->vm_start)/1024;	
+	printk("%016lx-%016lx %6uK \n", vma->vm_start, vma->vm_end, size);
+
+	if (node->rb_right) {
+		navigate_curr_subtree(root, node->rb_right);
+	}
+}
+
+void display_virtual_memory(struct task_struct *p)
+{
+	struct rb_root *root = &p->mm->mm_rb;
+	navigate_curr_subtree(root, root->rb_node); 
+}
+
+void rbtree_example(void) {
+	printk(KERN_NOTICE "---RBTREE example start---");
+
+	display_virtual_memory(current);
+
+	printk(KERN_NOTICE "---RBTREE example end---");
+}
 
 int init_module(void) 
 {
 
         printk(KERN_INFO "Lab3: start.\n");
 	kfifo_example();
+	rbtree_example();
         return 0;
 }
 
