2888d94b6946a8a751daa95a59314bc8d91108c7
Raman Buzaubakov <raman.buzaubakov@gmail.com>
2019-03-29 21:36:21 -0700

    week 8 lab

---
+++ week8/lab/lab7/lab-7.c
@@ -0,0 +1,38 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+
+static const char TAG[] = "Lab-7";
+
+
+struct tasklet_struct my_t;
+
+void tasklet_func(unsigned long data) {
+    printk(KERN_INFO "tasklet function is executed");
+}
+
+void post_init(void) {
+
+    tasklet_init(&my_t, tasklet_func, 123);
+    tasklet_schedule(&my_t);
+}
+
+static int __init lab_init(void)
+{
+    printk(KERN_INFO "%s init", TAG);
+
+    post_init();
+
+    return 0;
+}
+
+static void __exit lab_exit(void)
+{
+  tasklet_kill(&my_t);
+  printk(KERN_INFO "%s exit", TAG);
+
+}
+
+module_init(lab_init);
+module_exit(lab_exit);
+
